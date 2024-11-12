4fcc0013597269e2665d3c2a30b8c1158e0d93d5
Radka Pavlišová <153281974+RadkaPav@users.noreply.github.com>
2024-11-12 11:49:34 +0100

    Create otazka_1.sql

---
+++ otazka_1.sql
@@ -0,0 +1,5 @@
+-- Rostou v průběhu let mzdy ve všech odvětvích, nebo v některých klesají?
+
+SELECT year, ROUND(AVG(average_wages),0), industry FROM wages_food
+GROUP BY industry, year
+ORDER BY industry, year
